6a42ca64f7d759eab6e0f85e823a870787685926
David Groß <d.g1r2o3s4s@gmail.com>
2025-06-23 16:23:33 +0200

    added dishes.js

---
+++ scripts/dishes.js
@@ -0,0 +1,44 @@
+let dishes = {
+    "pizzas" : [
+        {
+            "name" : "Pizza Diavolo",
+            "description" : "mit Peperoniwurst, Oliven, grünen Peperoni, Zwiebeln und Knoblauch"
+        },
+        {
+            "name" : "Pizza Hawaii",
+            "description" : "mit Schinken und Ananas"
+        },
+        {
+            "name" : "Pizza vegetaria",
+            "description" : "mit frischem Gemüse"
+        }
+    ],
+    "noodles" : [
+        {
+            "name" : "Pasta Napoli",
+            "description" : "mit Tomatensauce"
+        },
+        {
+            "name" : "Pasta Diavolo",
+            "description" : "mit Zwiebeln, Oliven, grüner Peperoni, Peperonisalami und Knoblauch in Tomatensauce"
+        },
+        {
+            "name" : "Pasta Marinara",
+            "description" : "mit Meeresfrüchten"
+        }
+    ],
+    "salads" : [
+        {
+            "name" : "Der Knackige",
+            "description" : "gemischter Salat mit knusprigen Kartoffeln"
+        },
+        {
+            "name" : "Fitnesssalat",
+            "description" : "gemischter Salat mit gebratenen Hähnchenstreifen"
+        },
+        {
+            "name" : "Gemischter Salat",
+            "description" : "mit Tomaten, Gurken, Oliven, Mais und Paprika"
+        }
+    ]
+}
