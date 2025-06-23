3468713f573ed0876eac83a3a88089ed5ad48c9d
David Groß <d.g1r2o3s4s@gmail.com>
2025-06-23 19:43:27 +0200

    added dishes.js and template.js

--- scripts/dishes.js
+++ scripts/dishes.js
@@ -2,43 +2,52 @@ let dishes = {
     "pizzas" : [
         {
             "name" : "Pizza Diavolo",
-            "description" : "mit Peperoniwurst, Oliven, grünen Peperoni, Zwiebeln und Knoblauch"
+            "description" : "mit Peperoniwurst, Oliven, grünen Peperoni, Zwiebeln und Knoblauch",
+            "price" : 13.00
         },
         {
             "name" : "Pizza Hawaii",
-            "description" : "mit Schinken und Ananas"
+            "description" : "mit Schinken und Ananas",
+            "price" : 12.00
         },
         {
-            "name" : "Pizza vegetaria",
-            "description" : "mit frischem Gemüse"
+            "name" : "Pizza Vegetaria",
+            "description" : "mit frischem Gemüse",
+            "price" : 12.50
         }
     ],
     "noodles" : [
         {
             "name" : "Pasta Napoli",
-            "description" : "mit Tomatensauce"
+            "description" : "mit Tomatensauce",
+            "price" : 10.00
         },
         {
             "name" : "Pasta Diavolo",
-            "description" : "mit Zwiebeln, Oliven, grüner Peperoni, Peperonisalami und Knoblauch in Tomatensauce"
+            "description" : "mit Zwiebeln, Oliven, grüner Peperoni, Peperonisalami und Knoblauch in Tomatensauce",
+            "price" : 11.50
         },
         {
             "name" : "Pasta Marinara",
-            "description" : "mit Meeresfrüchten"
+            "description" : "mit Meeresfrüchten",
+            "price" : 14.00
         }
     ],
     "salads" : [
         {
             "name" : "Der Knackige",
-            "description" : "gemischter Salat mit knusprigen Kartoffeln"
+            "description" : "gemischter Salat mit knusprigen Kartoffeln",
+            "price" : 12.00
         },
         {
             "name" : "Fitnesssalat",
-            "description" : "gemischter Salat mit gebratenen Hähnchenstreifen"
+            "description" : "gemischter Salat mit gebratenen Hähnchenstreifen",
+            "price" : 13.50
         },
         {
             "name" : "Gemischter Salat",
-            "description" : "mit Tomaten, Gurken, Oliven, Mais und Paprika"
+            "description" : "mit Tomaten, Gurken, Oliven, Mais und Paprika",
+            "price" : 8.00
         }
     ]
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,12 +1,29 @@
-function gettemplate() {
-    `<div class="presentation_plate" id="content">
+function getDishesTemplate(section, indexDishes) {
+    return `<div class="presentation_plate">
                     <div class="description">
-                        <h3>Pizza Krabben</h3>
-                        <p>mit Krabben und Pepperoni</p>
-                        <p>9,50€</p>
+                        <h3>${dishes[section][indexDishes].name}</h3>
+                        <p>${dishes[section][indexDishes].description}</p>
+                        <p>${dishes[section][indexDishes].price.toFixed(2)} €</p>
                     </div>
-                    <button class="add">
+                    <button onclick="addToBasket('${section}',${indexDishes})" class="add">
                         <img src="./assets/icons/add.png" alt="">
                     </button>
                 </div>`
 }
+
+function getBasketTemplate(indexBasket) {
+    return `<div class="basket_item">
+                       <h4>${basketArray[indexBasket].name}</h4>
+                        <div class="item_price">
+                            <span>
+                            <button onclick="subtract(${indexBasket})"><img src="./assets/icons/minus.png" alt=""></button>
+                            <p id="multiply${indexBasket}">1</p>
+                            <button><img src="./assets/icons/plus.png" alt=""></button>
+                            </span>
+                            <p>${basketArray[indexBasket].price.toFixed(2)} €</p>
+                            <button onclick="deleteItem(${indexBasket})"><img src="./assets/icons/trashcan.png" alt=""></button>
+                        </div>
+                     </div>` 
+}
+
+
