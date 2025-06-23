a8bee88b24a238fa59d2d9e35aeb8df81c113a7c
David Groß <d.g1r2o3s4s@gmail.com>
2025-06-23 16:28:39 +0200

    added template.js

---
+++ scripts/templates.js
@@ -0,0 +1,12 @@
+function gettemplate() {
+    `<div class="presentation_plate" id="content">
+                    <div class="description">
+                        <h3>Pizza Krabben</h3>
+                        <p>mit Krabben und Pepperoni</p>
+                        <p>9,50€</p>
+                    </div>
+                    <button class="add">
+                        <img src="./assets/icons/add.png" alt="">
+                    </button>
+                </div>`
+}
